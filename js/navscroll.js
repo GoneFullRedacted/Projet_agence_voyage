147de3f47d24f1ea544aab2e9d1490de7275f47d
GoneFullRedacted <s.dauvergne58@gmail.com>
2025-04-28 16:53:00 +0200

    header + js

--- js/navscroll.js
+++ js/navscroll.js
@@ -4,9 +4,9 @@
   window.addEventListener("scroll", () => {
     if (window.scrollY > 0) {
       nav.classList.remove("bg-transparent");
-      nav.classList.add("bg-white");
+      nav.classList.add("bg-zinc-200");
     } else {
-      nav.classList.remove("bg-white");
+      nav.classList.remove("bg-zinc-200");
       nav.classList.add("bg-transparent");
     }
   });
